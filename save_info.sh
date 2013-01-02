59e51b8a46416cc89cd8351995725c54fd8e2fce
Paul Suh <plsuh@mbpc2d-wifi.goodeast.com>
2013-01-01 23:57:42 -0500

    fixed host-specific etc-APPEND in Flashbuilder.sh added .gitignore added save_info.sh which will create .tgz files with the system config

---
+++ save_info.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# capture keys & settings from a flashrd system
+
+# /etc/isakmpd
+# /etc/iked
+# /etc/ssh
+
+# /etc/rc.local
+# /etc/rc.conf.local
+# /etc/adduser.conf - from common
+# /etc/shells - from common
+# /etc/sudoers - from common
+
+# /usr/local
+
+cd /
+
+/bin/tar zcf /flash/`/bin/hostname`.tgz \
+	etc/isakmpd etc/iked etc/ssh \
+	etc/rc.local etc/rc.conf.local \
+	usr/local
+
+skipList="boot bsd bsd.mp etc new old openbsd.vnd var.tar `/bin/hostname`.tgz"
+FLASH_SAVE_DIRS=" "
+for oneDir in `/bin/ls /flash`; do
+	
+	echo "$skipList" | /usr/bin/grep $oneDir > /dev/null
+	if [[ $? -ne 0 ]]; then
+		echo $oneDir
+		FLASH_SAVE_DIRS="${FLASH_SAVE_DIRS} ${oneDir}"
+	fi
+	
+done
+
+cd /flash
+# $FLASH_SAVE_DIRS deliberately not quoted! 
+/bin/tar zcf /flash/`/bin/hostname`_flash.tgz ${FLASH_SAVE_DIRS}
+
+	
