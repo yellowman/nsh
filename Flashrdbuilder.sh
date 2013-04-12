61c2aee5bce7aeee86371fa2b0a80990cb7b8b09
Paul Suh <paul.suh@ps-enable.com>
2013-04-11 22:47:53 -0400

    Misc cleanup

--- Flashrdbuilder.sh
+++ Flashrdbuilder.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/ksh
 
 #
 # Flashrdbuilder.sh
--- save_info.sh
+++ save_info.sh
@@ -14,14 +14,15 @@
 
 # /usr/local
 
+hostname=`/bin/hostname`
 cd /
 
-/bin/tar zcf /flash/`/bin/hostname`.tgz \
+/bin/tar zcf "/flash/${hostname}.tgz" \
 	etc/isakmpd etc/iked etc/ssh \
 	etc/rc.local etc/rc.conf.local \
 	usr/local
 
-skipList="boot bsd bsd.mp etc new old openbsd.vnd var.tar `/bin/hostname`.tgz"
+skipList="boot bsd bsd.mp etc new old openbsd.vnd var.tar ${hostname}.tgz ${hostname}_flash.tgz"
 FLASH_SAVE_DIRS=" "
 for oneDir in `/bin/ls /flash`; do
 	
@@ -35,6 +36,6 @@ done
 
 cd /flash
 # $FLASH_SAVE_DIRS deliberately not quoted! 
-/bin/tar zcf /flash/`/bin/hostname`_flash.tgz ${FLASH_SAVE_DIRS}
+/bin/tar zcf "/flash/${hostname}_flash.tgz" ${FLASH_SAVE_DIRS}
 
-	
+
