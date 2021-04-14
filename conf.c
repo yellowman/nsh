779f7531e37d69c71d2d4d9e88b83eda383fc875
chris <chris@nmedia.net>
2021-04-13 18:29:05 -0700

    rid of unused stuff

--- conf.c
+++ conf.c
@@ -1287,7 +1287,6 @@ int
 isdefaultroute(struct sockaddr *sa, struct sockaddr *samask)
 {
 	struct sockaddr_in6 *sin6 = (struct sockaddr_in6 *)sa;
-	struct sockaddr_in6 *sin6mask = (struct sockaddr_in6 *)samask;
 
 	switch (sa->sa_family) {
 	case AF_INET:
