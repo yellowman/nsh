875343ddd9d50a6021dc35a95ff9ecbc028bd607
Stefan Sperling <stsp@stsp.name>
2023-05-25 07:04:37 +0000

    set version number back to 1.1
    To match OpenBSD versioning, 1.1 release is followed by 1.1-current,
    not vice versa.
    
    Suggested by Tom

--- nsh-version.mk
+++ nsh-version.mk
@@ -1,9 +1,9 @@
 # NSH_RELEASE is only set to "Yes" in a release tarball
 NSH_RELEASE=No
 
-# Version number of the next upcoming release.
-# Should be cranked immediately after publishing a release.
-NSH_VERSION_NUMBER=1.2
+# Version number of the most recently published release.
+# Should be cranked immediately before publishing a new release.
+NSH_VERSION_NUMBER=1.1
 
 .if ${NSH_RELEASE} == Yes
 NSH_VERSION=${NSH_VERSION_NUMBER}
