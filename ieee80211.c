ecce68ebb598685cd889290851bc9dd9378027ca
smytht <smytht@users.noreply.github.com>
2025-11-07 11:44:22 +0000

    ieee80211.c KNF reorder of Includes Sys std and net

--- ieee80211.c
+++ ieee80211.c
@@ -58,22 +58,27 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-#include <unistd.h>
-#include <errno.h>
-#include <stdarg.h>
-#include <sys/limits.h>
-#include <sys/socket.h>
 #include <sys/ioctl.h>
+#include <sys/limits.h>
 #include <sys/param.h>
+#include <sys/socket.h>
+
 #include <net/if.h>
+
 #include <netinet/in.h>
 #include <netinet/if_ether.h>
+
 #include <net80211/ieee80211.h>
 #include <net80211/ieee80211_ioctl.h>
+
+#include <ctype.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 #include "externs.h"
 
 const char *
