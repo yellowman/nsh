51ff2d1120018256ca5eacc680ad06e94fc112c4
smytht <smytht@users.noreply.github.com>
2025-11-07 11:30:49 +0000

    bridge.c KNF reorder of Includes Sys std and net

--- bridge.c
+++ bridge.c
@@ -26,27 +26,31 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <stdio.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <stdarg.h>
-#include <stdlib.h>
-#include <stdint.h>
+#include <sys/ioctl.h>
 #include <sys/param.h>
 #include <sys/socket.h>
-#include <sys/ioctl.h>
+#include <sys/types.h>
+
 #include <net/if.h>
 #include <net/if_dl.h>
 #include <net/if_types.h>
+
 #include <netinet/in.h>
 #include <netinet/if_ether.h>
+
 #include <net/if_bridge.h>
-#include <errno.h>
-#include <string.h>
+
 #include <err.h>
-#include <sysexits.h>
-#include <stdlib.h>
+#include <errno.h>
 #include <limits.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sysexits.h>
+#include <unistd.h>
+
 #include "externs.h"
 #include "bridge.h"
 
